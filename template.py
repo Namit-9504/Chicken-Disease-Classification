782e5aff8d71cccc94a18941e5792e49334942e1
Namit-9504 <namitchaudhari123@gmail.com>
2023-12-15 09:49:51 +0530

    folder structure added

---
+++ template.py
@@ -0,0 +1,43 @@
+import os 
+from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO,format='[%(asctim)s]:  %(message)s:')
+
+project_name = "cnnClassifier"
+list_of_files =[
+    ".github/workflows/.gitkeep0",
+    f"src/{project_name}/__init__.py",
+    f"src/{project_name}/components/__init__.py",
+    f"src/{project_name}/utils/__init__.py",
+    f"src/{project_name}/config/__init__.py",
+    f"src/{project_name}/config/configuration.py",
+    f"src/{project_name}/pipeline/__init__.py",
+    f"src/{project_name}/entity/__init__.py",
+    f"src/{project_name}/contansts/__init__.py",
+    "config/config.yaml",
+    "dvc.ymal",
+    "params.ymal",
+    "requirement.txt",
+    "setup.py",
+    "research/trails.ipynb"
+
+
+]
+
+
+for filepath in list_of_files:
+    filepath = Path(filepath)
+    filedir , filename = os.path.split(filepath)
+
+    if filedir !="":
+        os.makedirs(filedir,exist_ok=True)
+        logging.info(f"Creating Direactory;{filedir} for the file {filename}")
+    if (not os.path.exists(filepath)) or (os.path.getsize(filepath)==0):
+        with open(filepath ,"w") as f:
+            pass
+            logging.info(f"Creating Empty file:{filepath}")
+
+    else:
+        logging.info(f"{filename} i s already exist")
+
